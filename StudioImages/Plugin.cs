9a11b4bb773e6f34f20f56d37e1ac99fa496c313
Luke Pulverenti <luke.pulverenti@gmail.com>
2019-01-02 00:41:51 -0500

    initial

---
+++ StudioImages/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Collections;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Controller.Resolvers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Model.Drawing;
+using System.IO;
+
+namespace StudioImages
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin, IHasThumbImage
+    {
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "Studio Images"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get { return "Emby Studio Images"; }
+        }
+
+        private Guid _id = new Guid("C68856B8-6031-480D-B08E-43B9114ADDB2");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Stream GetThumbImage()
+        {
+            var type = GetType();
+            return type.Assembly.GetManifestResourceStream(type.Namespace + ".thumb.png");
+        }
+
+        public ImageFormat ThumbImageFormat
+        {
+            get
+            {
+                return ImageFormat.Png;
+            }
+        }
+    }
+}
